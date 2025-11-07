b6555485ae0faf44df0079ede39a54957f1664f7
LuucasRocha <lucasrocha@ucl.br>
2025-11-07 20:25:04 -0300

    add

--- src/fazerPedido.py
+++ src/fazerPedido.py
@@ -1,4 +1,10 @@
 from filaPedidos import filaPedidos
 
 def fazerPedido(pedido):
+    '''Exemplo: pedido = {
+                            "item": "Pizza Margherita",  # Nome do item
+                            "quantidade": 2             # Quantidade
+                         } '''
     filaPedidos.put(pedido)
+    
+    
--- src/processaPedido.py
+++ src/processaPedido.py
@@ -2,17 +2,28 @@ from filaPedidos import filaPedidos
 import json
 
 def processaPedido():
-    pedido = filaPedidos.get()
-    arq = open("./schema/cardapio.json", "r")
-    dadosCardapio = json.loads(arq.read())
-    arq.close()
+    pedido_atual = filaPedidos.get()
     try:
-        item_pedido = pedido["item"]
-        quantidade = pedido["quantidade"]
-        for item in dadosCardapio['itens']:
-            if item["nome"].lower() == item_pedido.lower():
-                preco_total = item["preco"] * quantidade
-                return {"status": 200, "mensagem": f"Pedido confirmado! Total: R$ {preco_total:.2f}", "detalhes": {"item": item["nome"], "quantidade": quantidade, "preco_unitario": item["preco"]}}
+        produto_solicitado = pedido_atual["item"]
+        qtd = pedido_atual["quantidade"]
+        if qtd <= 0:
+            return {"status": 400, "mensagem": "Quantidade deve ser maior que zero!"}
+        arq = open("../schema/cardapio.json", "r")
+        dados_do_cardapio = json.loads(arq.read())
+        arq.close()
+        for item_no_cardapio in dados_do_cardapio['itens']:
+            if item_no_cardapio["nome"].lower() == produto_solicitado.lower():
+                valor_unitario = item_no_cardapio["preco"]
+                total_a_pagar = valor_unitario * qtd
+                return {
+                    "status": 200,
+                    "mensagem": f"Pedido confirmado! Total: R$ {total_a_pagar:.2f}",
+                    "detalhes": {
+                        "item": item_no_cardapio["nome"],
+                        "quantidade": qtd,
+                        "preco_unitario": valor_unitario
+                    }
+                }
         return {"status": 404, "mensagem": "Item não encontrado no cardápio."}
     except KeyError:
         return {"status": 500, "mensagem": "Preencha todos os campos do pedido!"}
